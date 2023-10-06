31b06dc4e5ea9073f83f3eb137b47f82c5decc6e
Tomas R. Pita <tomasrpita@gmail.com>
2023-10-06 17:50:45 +0200

    373. Configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hola mundo pepe');
+const express = require('express');
+
+
+// crea el servidor de express
+const app = express();
+
+// Rutas
+app.get('/', (req, res) =>{
+
+    res.json({
+        ok: true
+    })
+})
+
+// escuchar peticiones
+const PORT = 4001;
+
+app.listen(PORT, () => {
+    console.log(`Serrvidor corriendo en el puerto ${PORT}`);
+});
