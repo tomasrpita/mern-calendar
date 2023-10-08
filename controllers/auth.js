7085535f8a2ef1b1abf87eebe35377594f1f49b3
Tomas R. Pita <tomasrpita@gmail.com>
2023-10-08 12:46:26 +0200

    377. Recuperar información de un posteo

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,17 +3,34 @@ const { response } = require('express');
 
 const crearUsuario = (req, res = response) =>{
 
-    res.json({
+    const {name, email, password} = req.body
+
+    if (name.length < 5) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'el nombre debe de ser minimo 5 letras'
+        })
+    }
+
+    res.status(201).json({
         ok: true,
-        msg: 'register'
+        msg: 'register',
+        name,
+        email,
+        password
     })
 }
 
 const loginUsuario = (req, res = response) =>{
 
+    const {name, email, password} = req.body
+
     res.json({
         ok: true,
-        msg: 'login'
+        msg: 'login',
+        name,
+        email,
+        password
     })
 }
 
--- index.js
+++ index.js
@@ -10,10 +10,13 @@ const app = express();
 // Directorio Püblico
 app.use(express.static('public')); // el path es el argumento
 
+
+// Lectura y parseo del Body que venga en json
+app.use(express.json())
+
+
 // Rutas
-
 app.use('/api/auth', require('./routes/auth'));
-
 // TODO: CRUD  -eventos Calendario
 
 // escuchar peticiones
