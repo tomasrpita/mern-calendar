ce9586badb233ea75bbfed9e640f9527823284c5
Tomas R. Pita <tomasrpita@gmail.com>
2023-10-23 05:34:06 +0200

    398. Actualizar un Evento

--- controllers/events.js
+++ controllers/events.js
@@ -11,7 +11,6 @@ const getEventos = async(req, res = Response ) => {
         eventos
     })
 
-
 }
 
 
@@ -42,12 +41,52 @@ const crearEvento = async(req, res = Response ) => {
 }
 
 
-const actualizarEvento = (req, res = Response ) => {
+const actualizarEvento = async(req, res = Response ) => {
 
-    res.json({
-        ok: true,
-        msg: 'actualizarEvento'
-    })
+    const eventoId = req.params.id;
+
+    try {
+        const evento = await Evento.findById( eventoId );
+
+        // Verificar si el evento existe
+        if ( !evento ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+
+        // Verificar si la persona que lo creo es la misma que lo quiere actualizar
+        if ( evento.user.toString() !== req.uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar este evento'
+            });
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: req.uid
+        }
+
+        // new: true, para que devuelva el evento actualizado
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } );
+
+        // Devolver el evento actualizado
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        })
+
+        
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 
 }
 
--- routes/events.js
+++ routes/events.js
@@ -33,6 +33,12 @@ router.post(
 // Actualizar evento
 router.put(
     '/:id',
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalización es obligatoria').custom( isDate ),
+        validarCampos
+    ],
     actualizarEvento
 );
 
